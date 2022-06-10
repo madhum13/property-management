a1723c4b500e28306de0d3268df5456e0baf55c5
madhum13 <madhumpura.1998@gmail.com>
2022-06-10 21:13:18 +0530

    updated commit

--- src/test/java/com/mycompany/propertymanagement/service/PropertyServiceImplTest.java
+++ src/test/java/com/mycompany/propertymanagement/service/PropertyServiceImplTest.java
@@ -14,6 +14,10 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 @ExtendWith(MockitoExtension.class)
 
 public class PropertyServiceImplTest {
@@ -47,6 +51,86 @@ public class PropertyServiceImplTest {
 
         PropertyDTO result = propertyServiceImpl.saveProperty(dto);
         Assertions.assertEquals(savedDTO.getId(), result.getId());
+    }
+
+    @Test
+    void testGetAllProperties_Success(){
+
+        List<PropertyEntity> propertyEntities = new ArrayList<>();
+        PropertyEntity propertyEntity = new PropertyEntity();
+        propertyEntity.setId(1L);
+        propertyEntity.setTitle("Dummy");
+        propertyEntities.add(propertyEntity);
+
+        PropertyDTO savedDTO = new PropertyDTO();
+        savedDTO.setTitle("Dummy");
+        savedDTO.setId(1L);
+
+        Mockito.when(propertyRepository.findAll()).thenReturn(propertyEntities);
+        Mockito.when(propertyConverter.convertEntityToDTO(Mockito.any())).thenReturn(savedDTO);
+
+       List<PropertyDTO> listPropertyDTO =  propertyServiceImpl.getAllProperties();
+
+       Assertions.assertEquals(1,listPropertyDTO.size());
+    }
+
+    void testUpdateProperty_Success(){
+
+        PropertyDTO savedDTO = new PropertyDTO();
+        savedDTO.setTitle("Dummy");
+        savedDTO.setId(1L);
+        savedDTO.setPrice(3443.43);
+        savedDTO.setAddress("wew");
+        savedDTO.setDescription("sasa");
+
+
+        PropertyEntity pe = new PropertyEntity();
+        pe.setTitle("Dummy");
+        pe.setId(1L);
+        savedDTO.setId(1L);
+        pe.setPrice(3443.43);
+        pe.setAddress("wew");
+        pe.setDescription("sasa");
+
+
+        Mockito.when(propertyRepository.findById(Mockito.anyLong())).thenReturn(Optional.of(pe));
+        Mockito.when(propertyConverter.convertEntityToDTO(Mockito.any())).thenReturn(savedDTO);
+
+        PropertyDTO updatedProperty = propertyServiceImpl.updateProperty(savedDTO, 1l);
+        Assertions.assertEquals(savedDTO.getTitle(), updatedProperty.getTitle());
+        Assertions.assertEquals(savedDTO.getPrice(), updatedProperty.getPrice());
+    }
+
+    @Test
+    void testUpdatePropertyDescription(){
+
+
+        PropertyDTO savedDTO = new PropertyDTO();
+
+        savedDTO.setDescription("updated q");
+
+        savedDTO.setTitle("Dummy");
+        savedDTO.setId(1L);
+        savedDTO.setPrice(3443.43);
+        savedDTO.setAddress("wew");
+        savedDTO.setDescription("sasa");
+
+        PropertyEntity pe = new PropertyEntity();
+        pe.setTitle("Dummy");
+        pe.setId(1L);
+        savedDTO.setId(1L);
+        pe.setPrice(3443.43);
+        pe.setAddress("wew");
+        pe.setDescription("updated q");
+
+        Mockito.when(propertyRepository.findById(Mockito.anyLong())).thenReturn(Optional.of(pe));
+        Mockito.when(propertyConverter.convertEntityToDTO(Mockito.any())).thenReturn(savedDTO);
+
+        propertyServiceImpl.updatePropertyDescription(savedDTO, 1L);
+
+
+        PropertyDTO updatedProperty = propertyServiceImpl.updatePropertyDescription(savedDTO, 1l);
+        Assertions.assertEquals(savedDTO.getDescription(), updatedProperty.getTitle());
     }
 
 }
